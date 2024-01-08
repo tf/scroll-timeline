04d410a4553cc672195e8e465f1ea05edfb1b89b
Tim Fischbach <tfischbach@codevise.de>
2024-01-08 13:09:51 +0100

    Only polyfill JS
    Remove processing of inline styles.

--- src/index.js
+++ src/index.js
@@ -23,19 +23,10 @@ import {
   ProxyAnimation
 } from "./proxy-animation.js";
 
-import { initCSSPolyfill } from "./scroll-timeline-css"
-
 function initPolyfill() {
-  // initCSSPolyfill returns true iff the host browser supports SDA
-  if (initCSSPolyfill()) {
+  // Don't load if browser claims support
+  if (CSS.supports("animation-timeline: --works")) {
     return;
-  }
-
-  if ([...document.styleSheets].filter((s) => s.href !== null).length) {
-    console.warn(
-      'Non-Inline StyleSheets detected: ScrollTimeline polyfill currently only' +
-        ' supports inline styles within style tags'
-    );
   }
 
   if (
